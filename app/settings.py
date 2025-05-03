1640a7fea841f10d9356701a425cd06d4a0a4cec
azamart <azamart@archlinuxlaptop>
2025-05-03 15:28:53 +0200

    update for Settings

--- app/settings.py
+++ app/settings.py
@@ -1,8 +1,5 @@
-import os
 # Define a folders structure, e.g. :
 # folders_dict = {'folder_name':[set of file extensions that you want to ut inside a folder]}
-
-
 folders_dict = {
     "photos": {"jpg", "jpeg", "png", "gif", "bmp", "heic"},
     "videos": {"mp4", "avi", "mkv", "mov"},
@@ -19,5 +16,3 @@ folders_dict = {
 path_to_upload = f'/app/static/uploads'
 hash_algo = 'md5'
 strong_password = "HelloWorld123!"
-strong_secret = os.urandom(24)
-
