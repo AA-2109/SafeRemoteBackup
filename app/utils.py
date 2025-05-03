918ac1f194ec05fb865e570c391b11b2edf51bc9
azamart <azamart@archlinuxlaptop>
2025-05-03 19:40:21 +0200

    bugfix

--- app/utils.py
+++ app/utils.py
@@ -29,7 +29,7 @@ def hash_file(filepath: str, algo: str = hash_algo):
 
 def get_relative_filepath(filepath: str):
     relative_path_str = filepath.split("/")[3:]
-    return "/"+"/".join(relative_path_str)
+    return f"{app.UPLOAD_DIR}"+"/".join(relative_path_str)
 
 
 def update_logfile(filepath: str, path_to_logfile: str, success: bool, e: Exception = None):
